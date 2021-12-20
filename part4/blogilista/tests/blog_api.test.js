19884d45219392bc842879e765a4e3d6e8dfe387
Ville_ThinkPad <ville.ponnelin@aalto.fi>
2021-12-20 16:55:19 +0200

    Create test for api get (4.8)

---
+++ part4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./api_helper')
+
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('correct number of blogs is found', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(6)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
