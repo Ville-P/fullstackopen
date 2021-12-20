0831dfa5dec513f4521db81191e3972ae21a901a
Ville_ThinkPad <ville.ponnelin@aalto.fi>
2021-12-20 12:18:31 +0200

    Create function for total likes (4.4)

--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let sumLikes = 0
+  blogs.forEach(blog => {
+    sumLikes += Number(blog.likes)
+  })
+  return sumLikes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
