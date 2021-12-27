99808bc9a8dc17e1609cdb338530156db6da47b7
Ville_ThinkPad <ville.ponnelin@aalto.fi>
2021-12-27 12:39:23 +0200

    Add full view option for blogs (5.7)

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 const Blog = ({blog}) => {
+  const [fullView, setFullView] = useState(false)
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -9,10 +10,21 @@ const Blog = ({blog}) => {
     marginBottom: 5
   }
 
-  return  (
-    <div style={blogStyle}>
-      {blog.title} {blog.author}
-    </div>
-)}
+  return fullView ?
+    (
+      <div style={blogStyle}>
+        {blog.title}
+        <button onClick={()  => setFullView(false)}>hide</button><br/>
+        {blog.url}<br/>
+        likes {blog.likes}<button>like</button><br/>
+        {blog.author}
+      </div>
+    ) : (
+      <div style={blogStyle}>
+        {blog.title}
+        <button onClick={()  => setFullView(true)}>view</button>
+      </div>
+    )
+}
 
 export default Blog
